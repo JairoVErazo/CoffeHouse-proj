a63ed625747299d1ea965e858856be764c023e37
Jairo Erazo <132089241+JairoVErazo@users.noreply.github.com>
2024-05-11 18:54:07 -0600

    Agregar archivos de proyecto.

---
+++ ReactApp2.Server/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers().RequireCors();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
