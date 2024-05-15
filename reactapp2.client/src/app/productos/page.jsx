31ddfda0be21f90ba69fb426f2a08086f3f05194
arrevillagamario <marrevillaga03@gmail.com>
2024-05-14 23:10:32 -0600

    prueba detalles

--- reactapp2.client/src/app/productos/page.jsx
+++ reactapp2.client/src/app/productos/page.jsx
@@ -45,6 +45,86 @@ const page = () => {
             </div>
           </div>
         </h2>
+<<<<<<< Updated upstream
+=======
+        {data.map((producto) => {
+          return (
+            <>
+              <div
+                style={{
+                  padding: "30px",
+                  backgroundColor: "#ffffff",
+                  borderRadius: "100px",
+                  marginBottom: "40px",
+                }}
+              >
+                <p
+                  style={{
+                    textAlign: "center",
+                    fontSize: "25px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {producto.nombreProducto}
+                </p>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <img
+                    src={"/img/cheesecake.jpg"}
+                    style={{
+                      width: "150px",
+                      height: "150px",
+                      border: "5px solid #94303c",
+                      marginLeft: "40px",
+                    }}
+                    alt="cake"
+                  />
+                  <div
+                    style={{
+                      textAlign: "center",
+                      fontSize: "25px",
+                      fontWeight: "bold",
+                      marginLeft: "100px",
+                    }}
+                  >
+                    <p>Costo</p>
+                    <p>Precio</p>
+                  </div>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      fontSize: "25px",
+                      fontWeight: "bold",
+                      marginLeft: "100px",
+                    }}
+                  >
+                    {producto.recetas.map((receta) => (
+                      <p>{receta.costoTotal}</p>
+                    ))}
+                    <p>${producto.precio}</p>
+                  </div>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      fontSize: "25px",
+                      fontWeight: "bold",
+                      marginLeft: "100px",
+                    }}
+                  >
+                    <p>Ingredientes</p>
+                    <div className="flex justify-center">
+                      <img
+                        src={"/img/UilSearchPlus.svg"}
+                        style={{ width: "50px" }}
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+
+>>>>>>> Stashed changes
         <div
           className="card rounded-xl"
           style={{
