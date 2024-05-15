50a94de9a4cc8aab9087ca905fb5ab635b14ccbe
GabrielRendon22 <143561068+GabrielRendon22@users.noreply.github.com>
2024-05-15 00:27:55 -0600

    ingredientes

--- ReactApp2.Server/Servicios/RepositorioIngredientes.cs
+++ ReactApp2.Server/Servicios/RepositorioIngredientes.cs
@@ -1,18 +1,52 @@
-﻿using CoffeHouse.Server.Models;
+﻿using AutoMapper;
+using CoffeHouse.Server.Dto_s;
+using CoffeHouse.Server.Models;
+using CoffeHouse.Server.Models.Custom;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoffeHouse.Server.Servicios
 {
     public interface IRepositorioIngredientes
     {
-
+        Task<Ingrediente> CrearIngrediente(Ingrediente ingrediente);
+        Task<IEnumerable<Ingrediente>> ObtenerIngredientes();
     }
     public class RepositorioIngredientes: IRepositorioIngredientes
     {
         private readonly CoffehouseContext _context;
-
-        public RepositorioIngredientes(CoffehouseContext context)
+        private readonly IMapper _mapper;
+        public RepositorioIngredientes(CoffehouseContext context,
+                                    IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<Ingrediente>> ObtenerIngredientes()
+        {
+
+            var ingredientes = await _context.Ingredientes.ToListAsync();
+
+
+            return ingredientes;
+        }
+
+        public async Task<Ingrediente> CrearIngrediente(  Ingrediente ingrediente )
+        {
+            
+
+            var nuevoIngrediente = new Ingrediente()
+            {
+                Nombre = ingrediente.Nombre,
+                Caducidad = ingrediente.Caducidad,
+                PrecioUnitario = ingrediente.PrecioUnitario,
+                Existencias = ingrediente.Existencias,         
+                UnidadMedida = ingrediente.UnidadMedida,
+            };
+
+            await _context.AddAsync(nuevoIngrediente);
+            await _context.SaveChangesAsync();
+
+            return nuevoIngrediente;
         }
     }
 }
