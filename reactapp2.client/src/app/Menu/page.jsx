e68b678a5ffc020b6f83b173fb244219147f2fba
arrevillagamario <marrevillaga03@gmail.com>
2024-06-10 11:39:08 -0600

    Revision de lo antes hecho

--- reactapp2.client/src/app/Menu/page.jsx
+++ reactapp2.client/src/app/Menu/page.jsx
@@ -5,8 +5,9 @@ import { useRouter } from "next/navigation";
 
 const Pagina = () => {
   const [datos, setDatos] = useState([]);
-  const [categoria, setCategoria] = useState(1);
-  const router= useRouter();
+    const [categoria, setCategoria] = useState(1);
+    const [productosSeleccionados, setProductosSeleccionados] = useState({});
+    const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,102 +25,130 @@ const Pagina = () => {
     fetchData();
   }, [categoria]);
 
-  console.log("Datos:", datos);
+    
 
-  return (
-    <div className="flex flex-col items-center py-10">
-      <ul className="font-bold flex p-4 mt-2 space-x-24 flex-row text-rose-900 text-4xl mb-20 list-disc cursor-pointer">
-        <li
-          className={`hover:text-pink-950 ${
-            categoria === 1 ? "bg-pink-200" : ""
-          }`}
-          onClick={() => setCategoria(1)}
-        >
-          Iced Coffee
-        </li>
-        <li
-          className={`hover:text-pink-950 ${
-            categoria === 2 ? "bg-pink-200" : ""
-          }`}
-          onClick={() => setCategoria(2)}
-        >
-          Hot Coffee
-        </li>
-        <li
-          className={`hover:text-pink-950 ${
-            categoria === 3 ? "bg-pink-200" : ""
-          }`}
-          onClick={() => setCategoria(3)}
-        >
-          Dessert
-        </li>
-        <li
-          className={`hover:text-pink-950 ${
-            categoria === 4 ? "bg-pink-200" : ""
-          }`}
-          onClick={() => setCategoria(4)}
-        >
-          Temporada
-        </li>
-        <li
-          className={`hover:text-pink-950 ${
-            categoria === 5 ? "bg-pink-200" : ""
-          }`}
-          onClick={() => setCategoria(5)}
-        >
-          Extras
-        </li>
-      </ul>
-      <div className="">
-        <div
-          className="flex ml-12"
-          style={{ maxWidth: "1300px", overflowX: "scroll" }}
-        >
-          {datos.length === 0 ? (
-            <p>No hay productos disponibles.</p>
-          ) : (
-            datos.map((producto) => (
-              <div className="pe-14 relative mb-20" key={producto.idProducto}>
-                <div className="absolute top-0 left-2">
-                  <button
-                    onClick={() =>
-                      router.push("/Menu/detalles/" + producto.idProducto)
-                    }
-                  >
-                    <img src="info.svg" alt="Icono Modal" className="h-8" />
-                  </button>
+    const agregarProducto = (producto) => {
+        const nuevoProducto = { ...productosSeleccionados };
+        nuevoProducto[producto.idProducto] = (nuevoProducto[producto.idProducto] || 0) + 1;
+        setProductosSeleccionados(nuevoProducto);
+    };
+
+    const quitarProducto = (producto) => {
+        const nuevoProducto = { ...productosSeleccionados };
+        if (nuevoProducto[producto.idProducto] > 1) {
+            nuevoProducto[producto.idProducto]--;
+        } else {
+            delete nuevoProducto[producto.idProducto];
+        }
+        setProductosSeleccionados(nuevoProducto);
+    };
+
+
+    return (
+        <div className="flex flex-col items-center py-10">
+            <ul className="font-bold flex p-4 mt-2 space-x-24 flex-row text-rose-900 text-4xl mb-20 list-disc cursor-pointer">
+                <li
+                    className={`hover:text-pink-950 ${categoria === 1 ? "bg-pink-200" : ""
+                        }`}
+                    onClick={() => setCategoria(1)}
+                >
+                    Iced Coffee
+                </li>
+                <li
+                    className={`hover:text-pink-950 ${categoria === 2 ? "bg-pink-200" : ""
+                        }`}
+                    onClick={() => setCategoria(2)}
+                >
+                    Hot Coffee
+                </li>
+                <li
+                    className={`hover:text-pink-950 ${categoria === 3 ? "bg-pink-200" : ""
+                        }`}
+                    onClick={() => setCategoria(3)}
+                >
+                    Dessert
+                </li>
+                <li
+                    className={`hover:text-pink-950 ${categoria === 4 ? "bg-pink-200" : ""
+                        }`}
+                    onClick={() => setCategoria(4)}
+                >
+                    Temporada
+                </li>
+                <li
+                    className={`hover:text-pink-950 ${categoria === 5 ? "bg-pink-200" : ""
+                        }`}
+                    onClick={() => setCategoria(5)}
+                >
+                    Extras
+                </li>
+            </ul>
+            <div className="">
+                <div
+                    className="flex ml-12"
+                    style={{ maxWidth: "1300px", overflowX: "scroll" }}
+                >
+                    {datos.length === 0 ? (
+                        <p>No hay productos disponibles.</p>
+                    ) : (
+                        datos.map((producto) => (
+                            <div className="pe-14 relative mb-20" key={producto.idProducto}>
+                                <div className="absolute top-0 left-2 cursor-pointer">
+                                    <button
+                                        onClick={() =>
+                                            router.push("/Menu/detalles/" + producto.idProducto)
+                                        }
+                                    >
+                                        <img src="info.svg" alt="Icono Modal" className="h-8" />
+                                    </button>
+                                </div>
+                                <div className="w-72 h-auto border border-gray-200 rounded-lg shadow">
+                                    <a href="#">
+                                        <img
+                                            src="img/menu1.png"
+                                            alt=""
+                                            className="h-70 rounded-t-lg"
+                                        />
+                                    </a>
+                                    <div className="p-5 bg-white">
+                                        <a href="#">
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-rose-900  justify-center text-center">
+                                                {producto.nombreProducto}
+                                            </h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-rose-900  justify-center text-center">
+                                                ${producto.precio}
+                                            </h5>
+                                        </a>
+                                        <div className="flex justify-center px-10 cursor-pointer">
+                                            <button
+                                                onClick={() => agregarProducto(producto)}
+                                                className="text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                                                style={{ backgroundColor: "#94303c" }}
+                                            >
+                                                +
+                                            </button>
+                                            {productosSeleccionados[producto.idProducto] && (
+                                                <button
+                                                    onClick={() => quitarProducto(producto)}
+                                                    className="text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                                                    style={{ backgroundColor: "#94303c" }}
+                                                >
+                                                    -
+                                                </button>
+                                            )}
+                                            <div className="px-3 text-rose-900 text-2xl">
+                                                {productosSeleccionados[producto.idProducto] || 0}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    )}
                 </div>
-                <div className="w-72 h-auto border border-gray-200 rounded-lg shadow">
-                  <a href="#">
-                    <img
-                      src="img/menu1.png"
-                      alt=""
-                      className="h-70 rounded-t-lg"
-                    />
-                  </a>
-                  <div className="p-5 bg-white">
-                    <a href="#">
-                      <h5 className="mb-2 text-xl font-bold tracking-tight text-rose-900 dark:text-white justify-center text-center">
-                        {producto.nombreProducto}
-                      </h5>
-                      <h5 className="mb-2 text-xl font-bold tracking-tight text-rose-900 dark:text-white justify-center text-center">
-                        ${producto.precio}
-                      </h5>
-                    </a>
-                    <div className="flex justify-center px-10">
-                      <img src="mas.svg" alt="" className="h-12" />
-                      <h1 className="px-12 text-rose-900 text-2xl">1</h1>
-                      <img src="menos.svg" alt="" className="h-12" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Pagina;
