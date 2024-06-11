4b0e087e52e8ec99359577e4baf67ab3d7449eaa
Kevin Daniel Martínez <140560743+RodriguezKevin8@users.noreply.github.com>
2024-06-10 23:28:26 -0600

    producto editar revision
    hola

--- reactapp2.client/src/app/productos/editar/[id]/page.jsx
+++ reactapp2.client/src/app/productos/editar/[id]/page.jsx
@@ -1,10 +1,16 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const page = ({ params }) => {
-  const [data, setData] = useState([]);
+const Page = ({ params }) => {
+  const [data, setData] = useState({
+    idCategoria: "",
+    nombreProducto: "",
+    deTemporada: false,
+    disponible: false,
+    descripcion: "",
+  });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -16,9 +22,40 @@ const page = ({ params }) => {
     };
 
     fetchData();
-  }, []);
+  }, [params.id]);
 
-  console.log(data);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value === "true" ? true : value === "false" ? false : value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const dataToSend = new FormData();
+    dataToSend.append("idCategoria", data.idCategoria);
+    dataToSend.append("nombreProducto", data.nombreProducto);
+    dataToSend.append("descripcion", data.descripcion);
+    dataToSend.append("disponible", data.disponible);
+    dataToSend.append("deTemporada", data.deTemporada);
+
+    try {
+        console.log("Datos antes de enviar", dataToSend);
+        const response = await axios.put(`/api/Productos${params.id}`, dataToSend, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("Data updated successfully:", response.dataToSend);
+      alert("Datos editados correctamente");
+    } catch (error) {
+      console.error("Error updating data:", error);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center">
       <div style={{ backgroundColor: "#bb8b90" }} className="rounded-lg ">
@@ -28,36 +65,44 @@ const page = ({ params }) => {
             Nombre del producto
           </h2>
         </div>
-        <form className="mt-5 flex flex-col  items-center mb-10">
+        <form
+          className="mt-5 flex flex-col items-center mb-10"
+          onSubmit={handleSubmit}
+        >
           <div className="flex space-x-10">
             <div>
               <div className="flex flex-col ">
                 <label
-                  htmlFor=""
-                  className="uppercase  text-white font-extrabold"
+                  htmlFor="idCategoria"
+                  className="uppercase text-white font-extrabold"
                 >
                   IdCategoria
                 </label>
                 <input
                   type="text"
+                  name="idCategoria"
                   className="w-80 h-8 rounded-lg border-none px-6"
                   style={{ backgroundColor: "#dfdfdf" }}
                   value={data.idCategoria}
+                  onChange={handleChange}
+                  disabled
                 />
               </div>
             </div>
             <div className="flex flex-col ">
               <label
-                htmlFor=""
-                className="uppercase  text-white font-extrabold "
+                htmlFor="nombreProducto"
+                className="uppercase text-white font-extrabold"
               >
                 Nombre
               </label>
               <input
                 type="text"
+                name="nombreProducto"
                 className="w-80 h-8 rounded-lg border-none px-6"
                 style={{ backgroundColor: "#dfdfdf" }}
                 value={data.nombreProducto}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -66,32 +111,40 @@ const page = ({ params }) => {
             <div>
               <div className="flex flex-col ">
                 <label
-                  htmlFor=""
-                  className="uppercase  text-white font-extrabold"
+                  htmlFor="deTemporada"
+                  className="uppercase text-white font-extrabold"
                 >
                   Temporada
                 </label>
-                <input
-                  type="text"
+                <select
+                  name="deTemporada"
                   className="w-80 h-8 rounded-lg border-none px-6"
                   style={{ backgroundColor: "#dfdfdf" }}
                   value={data.deTemporada}
-                />
+                  onChange={handleChange}
+                >
+                  <option value="true">Sí</option>
+                  <option value="false">No</option>
+                </select>
               </div>
             </div>
             <div className="flex flex-col ">
               <label
-                htmlFor=""
-                className="uppercase  text-white font-extrabold"
+                htmlFor="disponible"
+                className="uppercase text-white font-extrabold"
               >
                 Disponible
               </label>
-              <input
-                type="text"
+              <select
+                name="disponible"
                 className="w-80 h-8 rounded-lg border-none px-6"
                 style={{ backgroundColor: "#dfdfdf" }}
                 value={data.disponible}
-              />
+                onChange={handleChange}
+              >
+                <option value="true">Sí</option>
+                <option value="false">No</option>
+              </select>
             </div>
           </div>
 
@@ -99,16 +152,18 @@ const page = ({ params }) => {
             <div>
               <div className="flex flex-col mr-72">
                 <label
-                  htmlFor=""
-                  className="uppercase  text-white font-extrabold"
+                  htmlFor="descripcion"
+                  className="uppercase text-white font-extrabold"
                 >
                   Descripción
                 </label>
                 <input
                   type="text"
+                  name="descripcion"
                   className="w-96 h-20 rounded-lg border-none px-6"
                   style={{ backgroundColor: "#dfdfdf" }}
                   value={data.descripcion}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -126,4 +181,4 @@ const page = ({ params }) => {
   );
 };
 
-export default page;
+export default Page;
