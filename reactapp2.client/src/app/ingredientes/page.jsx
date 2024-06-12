526de2342ab04a6c7357ff38f257e1abacbe2991
arrevillagamario <marrevillaga03@gmail.com>
2024-06-12 15:42:31 -0600

    Rutas 2 protegidas

--- reactapp2.client/src/app/ingredientes/page.jsx
+++ reactapp2.client/src/app/ingredientes/page.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-
+import ProtectedRoute from "../ProtectedRoute";
 const page = () => {
   const router = useRouter();
 
@@ -23,98 +23,100 @@ const page = () => {
 
   console.log(data);
   return (
-    <div className="flex justify-center" style={{ color: "#94303c" }}>
-      <div
-        className="flex mb-56 mx-10 ancho-cocina mt-5"
-        style={{ maxHeight: "600px", overflowY: "scroll" }}
-      >
-        <div>
-          <div
-            className="col-md-6 py-16 rounded-md px-40"
-            style={{ backgroundColor: "#f7f6f6" }}
-          >
-            <h2
-              className="text-center"
-              style={{
-                fontSize: "45px",
-                fontWeight: "bold",
-                marginLeft: "40px",
-              }}
-            >
-              <div className="flex justify-center">
-                <div>
-                  <h1>Ingredientes</h1>
-                </div>
-                <div>
-                  <button
-                    className="ml-5"
-                    onClick={() => router.push("/ingredientes/new")}
-                  >
-                    <img src="mas.svg" alt="info" className="h-10" />
-                  </button>
-                </div>
-              </div>
-            </h2>
+    <ProtectedRoute>
+      <div className="flex justify-center" style={{ color: "#94303c" }}>
+        <div
+          className="flex mb-56 mx-10 ancho-cocina mt-5"
+          style={{ maxHeight: "600px", overflowY: "scroll" }}
+        >
+          <div>
             <div
-              className="card rounded-xl"
-              style={{
-                marginBottom: "20px",
-                backgroundColor: "#ffffff, 0.7",
-                padding: "50px",
-              }}
+              className="col-md-6 py-16 rounded-md px-40"
+              style={{ backgroundColor: "#f7f6f6" }}
             >
-              <div className="card-body">
-                <div className="details space-y-11">
-                  {data.map((ingrediente) => (
-                    <div
-                      key={ingrediente.idIngrediente}
-                      style={{
-                        padding: "30px",
-                        backgroundColor: "#ffffff",
-                        borderRadius: "100px",
-                      }}
+              <h2
+                className="text-center"
+                style={{
+                  fontSize: "45px",
+                  fontWeight: "bold",
+                  marginLeft: "40px",
+                }}
+              >
+                <div className="flex justify-center">
+                  <div>
+                    <h1>Ingredientes</h1>
+                  </div>
+                  <div>
+                    <button
+                      className="ml-5"
+                      onClick={() => router.push("/ingredientes/new")}
                     >
-                      <div className="flex justify-center">
-                        <div className="ml-60">
-                          <p
-                            style={{
-                              textAlign: "center",
-                              fontSize: "30px",
-                              fontWeight: "bold",
-                            }}
-                          >
-                            {ingrediente.nombre}
-                          </p>
-                        </div>
-                      </div>
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <div className="justify-center flex text-xl font-bold">
-                          <div className="mr-20 ml-10">
-                            <p className="text-center mr-10 ml-6">
-                              Precio Unitario:
-                            </p>
-                            <p className="text-center">
-                              ${ingrediente.precioUnitario}
+                      <img src="mas.svg" alt="info" className="h-10" />
+                    </button>
+                  </div>
+                </div>
+              </h2>
+              <div
+                className="card rounded-xl"
+                style={{
+                  marginBottom: "20px",
+                  backgroundColor: "#ffffff, 0.7",
+                  padding: "50px",
+                }}
+              >
+                <div className="card-body">
+                  <div className="details space-y-11">
+                    {data.map((ingrediente) => (
+                      <div
+                        key={ingrediente.idIngrediente}
+                        style={{
+                          padding: "30px",
+                          backgroundColor: "#ffffff",
+                          borderRadius: "100px",
+                        }}
+                      >
+                        <div className="flex justify-center">
+                          <div className="ml-60">
+                            <p
+                              style={{
+                                textAlign: "center",
+                                fontSize: "30px",
+                                fontWeight: "bold",
+                              }}
+                            >
+                              {ingrediente.nombre}
                             </p>
                           </div>
-                          <div>
-                            <p className="text-center">Existencias:</p>
-                            <p className="text-center">
-                              {ingrediente.existencias}{" "}
-                              {ingrediente.unidadMedida}
-                            </p>
+                        </div>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          <div className="justify-center flex text-xl font-bold">
+                            <div className="mr-20 ml-10">
+                              <p className="text-center mr-10 ml-6">
+                                Precio Unitario:
+                              </p>
+                              <p className="text-center">
+                                ${ingrediente.precioUnitario}
+                              </p>
+                            </div>
+                            <div>
+                              <p className="text-center">Existencias:</p>
+                              <p className="text-center">
+                                {ingrediente.existencias}{" "}
+                                {ingrediente.unidadMedida}
+                              </p>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 };
 
--- reactapp2.client/src/app/orden/page.jsx
+++ reactapp2.client/src/app/orden/page.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import swal from "sweetalert";
-
+import ProtectedRoute from "../ProtectedRoute";
 const Page = () => {
   const cart = useStore((state) => state.cart);
   const idOrden = useStore((state) => state.idOrden);
@@ -62,97 +62,99 @@ const Page = () => {
   };
 
   return (
-    <div>
-      <h2
-        className="text-center"
-        style={{
-          fontSize: "45px",
-          fontWeight: "bold",
-          marginLeft: "40px",
-        }}
-      >
-        <div className="text-center">
-          <h1>Verifica tu orden</h1>
-        </div>
-      </h2>
+    <ProtectedRoute>
+      <div>
+        <h2
+          className="text-center"
+          style={{
+            fontSize: "45px",
+            fontWeight: "bold",
+            marginLeft: "40px",
+          }}
+        >
+          <div className="text-center">
+            <h1>Verifica tu orden</h1>
+          </div>
+        </h2>
 
-      {data.length === 0 ? (
-        <p className="text-center ps-11">No hay productos en la orden.</p>
-      ) : (
-        <>
-          <ul>
-            {data.map((producto) => (
-              <li key={producto.idProducto}>
-                <div className="flex justify-center">
-                  <div className="col-md-6 py-2">
-                    <div className="flex justify-self-start">
-                      <div
-                        className="card rounded-xl"
-                        style={{
-                          backgroundColor: "#ffffff, 0.7",
-                          padding: "5px",
-                          width: "850px",
-                        }}
-                      >
-                        <div className="card-body">
-                          <div className="details space-y-11">
-                            <div
-                              style={{
-                                padding: "30px",
-                                backgroundColor: "#ffffff",
-                                borderRadius: "100px",
-                              }}
-                            >
+        {data.length === 0 ? (
+          <p className="text-center ps-11">No hay productos en la orden.</p>
+        ) : (
+          <>
+            <ul>
+              {data.map((producto) => (
+                <li key={producto.idProducto}>
+                  <div className="flex justify-center">
+                    <div className="col-md-6 py-2">
+                      <div className="flex justify-self-start">
+                        <div
+                          className="card rounded-xl"
+                          style={{
+                            backgroundColor: "#ffffff, 0.7",
+                            padding: "5px",
+                            width: "850px",
+                          }}
+                        >
+                          <div className="card-body">
+                            <div className="details space-y-11">
                               <div
                                 style={{
-                                  display: "flex",
-                                  alignItems: "center",
+                                  padding: "30px",
+                                  backgroundColor: "#ffffff",
+                                  borderRadius: "100px",
                                 }}
                               >
-                                <img
-                                  src={"/img/cheesecake.jpg"}
-                                  style={{
-                                    width: "150px",
-                                    height: "150px",
-                                    border: "5px solid #94303c",
-                                    marginLeft: "40px",
-                                  }}
-                                  alt="cake"
-                                />
                                 <div
                                   style={{
-                                    textAlign: "center",
-                                    fontSize: "25px",
-                                    fontWeight: "bold",
-                                    marginLeft: "40px",
+                                    display: "flex",
+                                    alignItems: "center",
                                   }}
                                 >
+                                  <img
+                                    src={"/img/cheesecake.jpg"}
+                                    style={{
+                                      width: "150px",
+                                      height: "150px",
+                                      border: "5px solid #94303c",
+                                      marginLeft: "40px",
+                                    }}
+                                    alt="cake"
+                                  />
                                   <div
                                     style={{
-                                      display: "flex",
-                                      alignItems: "center",
+                                      textAlign: "center",
+                                      fontSize: "25px",
+                                      fontWeight: "bold",
+                                      marginLeft: "40px",
                                     }}
                                   >
-                                    <p style={{ marginRight: "20px" }}>
-                                      {producto.nombreProducto}
-                                    </p>
-                                    <p
+                                    <div
                                       style={{
-                                        marginRight: "30px",
-                                        padding: "3px",
-                                        border: "2px solid #94303c",
+                                        display: "flex",
+                                        alignItems: "center",
                                       }}
                                     >
-                                      {cart[producto.idProducto]}
-                                    </p>
+                                      <p style={{ marginRight: "20px" }}>
+                                        {producto.nombreProducto}
+                                      </p>
+                                      <p
+                                        style={{
+                                          marginRight: "30px",
+                                          padding: "3px",
+                                          border: "2px solid #94303c",
+                                        }}
+                                      >
+                                        {cart[producto.idProducto]}
+                                      </p>
 
-                                    <p style={{ marginRight: "30px" }}>
-                                      Total: $
-                                      {(
-                                        producto.precio *
-                                        cart[producto.idProducto]
-                                      ).toFixed(2)}
-                                    </p>
+                                      <p style={{ marginRight: "30px" }}>
+                                        Total: $
+                                        {(
+                                          producto.precio *
+                                          cart[producto.idProducto]
+                                        ).toFixed(2)}
+                                      </p>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
@@ -162,39 +164,41 @@ const Page = () => {
                       </div>
                     </div>
                   </div>
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-center mt-4">
+              {idOrden && (
+                <div className="text-black font-bold">
+                  ID de la Orden: {idOrden}
                 </div>
-              </li>
-            ))}
-          </ul>
-          <div className="flex justify-center mt-4">
-            {idOrden && (
+              )}
+            </div>
+
+            <div className="flex justify-center mt-4">
               <div className="text-black font-bold">
-                ID de la Orden: {idOrden}
+                Total a Pagar: ${total}
               </div>
-            )}
-          </div>
+            </div>
+            {localStorage.setItem("precio", total)}
 
-          <div className="flex justify-center mt-4">
-            <div className="text-black font-bold">Total a Pagar: ${total}</div>
-          </div>
-          {localStorage.setItem("precio", total)}
-
-          <div className="flex justify-center">
-            <button
-              type="button"
-              onClick={handleConfirmarPedido}
-              className="text-white  font-medium rounded-lg text-sm px-5 py-2.5 me-16 mb-2 my-4 "
-              style={{
-                backgroundColor: "#94303c",
-                marginLeft: "80px",
-              }}
-            >
-              Confirmar pedido
-            </button>
-          </div>
-        </>
-      )}
-    </div>
+            <div className="flex justify-center">
+              <button
+                type="button"
+                onClick={handleConfirmarPedido}
+                className="text-white  font-medium rounded-lg text-sm px-5 py-2.5 me-16 mb-2 my-4 "
+                style={{
+                  backgroundColor: "#94303c",
+                  marginLeft: "80px",
+                }}
+              >
+                Confirmar pedido
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </ProtectedRoute>
   );
 };
 
